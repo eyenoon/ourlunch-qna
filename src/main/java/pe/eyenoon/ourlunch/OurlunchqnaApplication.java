10301f340627871353fa8e1a51b8f51eebdacd80
BangSeongCheol <40446418+BangSeongCheol@users.noreply.github.com>
2019-08-08 01:51:49 +0900

    빌드 셋팅

---
+++ src/main/java/pe/eyenoon/ourlunch/OurlunchqnaApplication.java
@@ -0,0 +1,11 @@
+package pe.eyenoon.ourlunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurlunchqnaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OurlunchqnaApplication.class, args);
+    }
+}
---
+++ src/main/java/pe/eyenoon/ourlunch/controller/BoardController.java
@@ -0,0 +1,13 @@
+package pe.eyenoon.ourlunch.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+
+    @RequestMapping("/")
+    public String board() {
+        return "Hello, World!!";
+    }
+}
